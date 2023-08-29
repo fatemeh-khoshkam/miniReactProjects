d8d400ab183d0bf6f7cd516fdd5bb69eb9c9bfec
fatemeh khoshkam <fa.khoshkam@gmail.com>
2023-08-29 22:22:53 +0330

    add static version of accordian

---
+++ 06-accordian/src/App.js
@@ -0,0 +1,47 @@
+import "./index.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} key={i} num={i} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ title, text, num }) {
+  return (
+    <div className="item">
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">-</p>
+      <div className="content-box">{text}</div>
+    </div>
+  );
+}
+
+export default App;
