7aef4615e7d9b635266843c8a18003513a6158cc
fatemeh khoshkam <fa.khoshkam@gmail.com>
2023-09-07 23:46:52 +0330

    finish ui of project 🍿

--- 09-usepopcorn/src/App.js
+++ 09-usepopcorn/src/App.js
@@ -1,10 +1,154 @@
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
 
-function App() {
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+export default function App() {
   return (
-    <div>
-      new project
-    </div>
+    <>
+      <Nav></Nav>
+      <Content></Content>
+    </>
   );
 }
 
-export default App;
+function Nav() {
+  return (
+    <nav className="nav-bar">
+      <div className="logo">
+        <span role="img">🍿</span>
+        <h1>usePopcorn</h1>
+      </div>
+      <input className="search" type="text" placeholder="Search movies ..." />
+      <p className="num-results">
+        Found <strong>3</strong> results
+      </p>
+    </nav>
+  );
+}
+
+function ToggleBtn() {
+  return <button className="btn-toggle">+</button>;
+}
+
+function Content() {
+  const movies = tempMovieData;
+  const watchedmovies = tempWatchedData;
+  return (
+    <main className="main">
+      <div className="box">
+        <ToggleBtn></ToggleBtn>
+        <ul className="list">
+          {movies.length > 0 ? (
+            movies.map((movie) => (
+              <li>
+                <img src={movie.Poster} alt={`${movie.Title} poster`} />
+                <h3>{movie.Title}</h3>
+                <div>
+                  <p>
+                    <span>🗓</span>
+                    <span>{movie.Year}</span>
+                  </p>
+                </div>
+              </li>
+            ))
+          ) : (
+            <strong>You don't have any movies yet 🎬</strong>
+          )}
+        </ul>
+      </div>
+
+      <div className="box">
+        <ToggleBtn></ToggleBtn>
+        <>
+          <div className="summary">
+            <h2>Movies you watched</h2>
+            <div>
+              <p>
+                <span>#️⃣</span>
+                <span>{watchedmovies.length} movies</span>
+              </p>
+              <p>
+                <span>⭐️</span>
+                <span>6</span>
+              </p>
+              <p>
+                <span>🌟</span>
+                <span>3</span>
+              </p>
+              <p>
+                <span>⏳</span>
+                <span>1 min</span>
+              </p>
+            </div>
+          </div>
+
+          <ul className="list">
+            {watchedmovies.map((movie) => (
+              <li>
+                <img src={movie.Poster} alt={`${movie.Title} poster`} />
+                <h3>{movie.Title}</h3>
+                <div>
+                  <p>
+                    <span>⭐️</span>
+                    <span>{movie.imdbRating}</span>
+                  </p>
+                  <p>
+                    <span>🌟</span>
+                    <span>{movie.userRating}</span>
+                  </p>
+                  <p>
+                    <span>⏳</span>
+                    <span>{movie.runtime} min</span>
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </>
+      </div>
+    </main>
+  );
+}
