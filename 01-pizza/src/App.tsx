ce88f6f6bb95b059cc93a2c7e1258b558be6654b
fatemeh khoshkam <fa.khoshkam@gmail.com>
2024-06-23 22:36:36 +0300

    Changes to TypeScript

---
+++ 01-pizza/src/App.tsx
@@ -0,0 +1,148 @@
+import "./index.css";
+
+//Lists of pizza dishes
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 16,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 18,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 13,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 25,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
+
+//Header of menu
+function Header() {
+    return (
+        <header className="header">
+            <h1>Fast React Pizza Co.</h1>
+    </header>
+);
+}
+
+function Menu() {
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
+
+    return (
+        <main className="menu">
+            <h2>Our menu</h2>
+
+    {/* If you have pizza in data, you can see that list , if not have any pizza on that list you able to see paragraph */}
+    {numPizzas > 0 ? (
+        //React Fragment
+        <>
+            <p>
+                Authentic Italian cuisine. 6 creative dishes to choose from. All
+        from our stone oven, all organic, all delicious.
+    </p>
+
+    <ul className="pizzas">
+        {pizzas.map((pizza) => (
+                <Pizza pizzaObj={pizza} key={pizza.name} />
+    ))}
+        </ul>
+        </>
+    ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+    )}
+    </main>
+);
+}
+
+function Pizza({ pizzaObj }) {
+    const soldOut = pizzaObj.soldOut;
+    //If one item of pizza sold out, that color is changed and that prices is changed and replaced with text "SOLD OUT"
+    return (
+        <li className={`pizza ${soldOut ? "sold-out" : ""}`}>
+    <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+    <div>
+    <h3>{pizzaObj.name}</h3>
+    <p>{pizzaObj.ingredients}</p>
+    <span>{soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+    </div>
+    </li>
+);
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+
+    //OPEN restuarant betwwen 12 untill 22
+    return (
+        <footer className="footer">
+            {isOpen ? (
+                    <Order closeHour={closeHour} openHour={openHour} />
+) : (
+        <p>
+            We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+    </p>
+)}
+    </footer>
+);
+}
+
+// If you want to see order button, you just go between 12 and 22 hour
+function Order({ closeHour, openHour }) {
+    return (
+        <div className="order">
+            <p>
+                We're open from {openHour}:00 until {closeHour}:00. Come visit us or
+    order online.
+    </p>
+    <button className="btn">Order</button>
+        </div>
+);
+}
+
+//Main component
+function App() {
+    return (
+        <div className="container">
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
