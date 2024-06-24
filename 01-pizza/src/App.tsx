04ab9bd6c4f11d6ffe2d29b3e9edfb3b8a2d398a
fatemeh khoshkam <fa.khoshkam@gmail.com>
2024-06-24 21:10:07 +0300

    Refactor

--- 01-pizza/src/App.tsx
+++ 01-pizza/src/App.tsx
@@ -1,54 +1,64 @@
-import "./index.css";
+import "./assets/styles/index.css";
+import React from "react";
+import imagePaths from "./imagePaths";
 
+interface pizzaDataType {
+    name:string,
+    ingredients:string,
+    price:number,
+    photoName:string,
+    soldOut:boolean
+}
 //Lists of pizza dishes
-const pizzaData = [
+const pizzaData:pizzaDataType[] = [
     {
         name: "Focaccia",
         ingredients: "Bread with italian olive oil and rosemary",
         price: 16,
-        photoName: "pizzas/focaccia.jpg",
+        photoName: "focaccia",
         soldOut: false,
     },
     {
         name: "Pizza Margherita",
         ingredients: "Tomato and mozarella",
         price: 18,
-        photoName: "pizzas/margherita.jpg",
+        photoName: "margherita",
         soldOut: true,
     },
     {
         name: "Pizza Spinaci",
         ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
         price: 13,
-        photoName: "pizzas/spinaci.jpg",
+        photoName: "spinaci",
         soldOut: false,
     },
     {
         name: "Pizza Funghi",
         ingredients: "Tomato, mozarella, mushrooms, and onion",
         price: 25,
-        photoName: "pizzas/funghi.jpg",
+        photoName: "funghi",
         soldOut: false,
     },
     {
         name: "Pizza Salamino",
         ingredients: "Tomato, mozarella, and pepperoni",
         price: 15,
-        photoName: "pizzas/salamino.jpg",
+        photoName: "salamino",
         soldOut: true,
     },
     {
         name: "Pizza Prosciutto",
         ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
         price: 18,
-        photoName: "pizzas/prosciutto.jpg",
+        photoName: "prosciutto",
         soldOut: false,
     },
 ];
 
 
+
 //Header of menu
-function Header() {
+function Header(): React.JSX.Element {
     return (
         <header className="header">
             <h1>Fast React Pizza Co.</h1>
@@ -56,10 +66,9 @@ function Header() {
 );
 }
 
-function Menu() {
-    const pizzas = pizzaData;
-    const numPizzas = pizzas.length;
-
+function Menu():React.JSX.Element {
+    const pizzas:pizzaDataType[] = pizzaData;
+    const numPizzas:number = pizzas.length;
     return (
         <main className="menu">
             <h2>Our menu</h2>
@@ -86,12 +95,14 @@ function Menu() {
 );
 }
 
-function Pizza({ pizzaObj }) {
-    const soldOut = pizzaObj.soldOut;
+function Pizza({ pizzaObj }: { pizzaObj: pizzaDataType }) {
+
+    const imagePath:string = imagePaths[pizzaObj.photoName];
+    const soldOut:boolean = pizzaObj.soldOut;
     //If one item of pizza sold out, that color is changed and that prices is changed and replaced with text "SOLD OUT"
     return (
         <li className={`pizza ${soldOut ? "sold-out" : ""}`}>
-    <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+    <img src={imagePath} alt={pizzaObj.name} />
     <div>
     <h3>{pizzaObj.name}</h3>
     <p>{pizzaObj.ingredients}</p>
@@ -101,13 +112,13 @@ function Pizza({ pizzaObj }) {
 );
 }
 
-function Footer() {
-    const hour = new Date().getHours();
-    const openHour = 12;
-    const closeHour = 22;
-    const isOpen = hour >= openHour && hour <= closeHour;
+function Footer():React.JSX.Element {
+    const hour:number = new Date().getHours();
+    const openHour:number = 12;
+    const closeHour:number = 22;
+    const isOpen:boolean = hour >= openHour && hour <= closeHour;
 
-    //OPEN restuarant betwwen 12 untill 22
+    //OPEN restaurant between 12 until 22
     return (
         <footer className="footer">
             {isOpen ? (
@@ -122,7 +133,7 @@ function Footer() {
 }
 
 // If you want to see order button, you just go between 12 and 22 hour
-function Order({ closeHour, openHour }) {
+function Order({ closeHour, openHour } : {closeHour:number , openHour:number }):React.JSX.Element {
     return (
         <div className="order">
             <p>
@@ -135,7 +146,7 @@ function Order({ closeHour, openHour }) {
 }
 
 //Main component
-function App() {
+function App():React.JSX.Element {
     return (
         <div className="container">
             <Header />
---
+++ 01-pizza/src/imagePaths.ts
@@ -0,0 +1,17 @@
+import focaccia from './assets/images/focaccia.jpg';
+import margherita from './assets/images/margherita.jpg';
+import funghi from './assets/images/funghi.jpg';
+import spinaci from './assets/images/spinaci.jpg';
+import prosciutto from './assets/images/prosciutto.jpg';
+import salamino from './assets/images/salamino.jpg';
+
+const imagePaths: { [key: string]: string } = {
+    focaccia,
+    margherita,
+    funghi,
+    spinaci,
+    prosciutto,
+    salamino
+};
+
+export default imagePaths;
