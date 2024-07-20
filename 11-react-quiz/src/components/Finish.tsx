5d5399a8062d1268d5e3c055c6f5aca773bc9d2c
fatemeh khoshkam <fa.khoshkam@gmail.com>
2024-07-20 21:19:55 +0300

    WIP: Based on percentage changes the emoji format

--- 11-react-quiz/src/components/Finish.tsx
+++ 11-react-quiz/src/components/Finish.tsx
@@ -6,8 +6,17 @@ type finishPropsType = {
 
 function Finish({ points, maxPoints }: finishPropsType) {
   const percentage: number = Math.ceil((points / maxPoints) * 100);
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage >= 0 && percentage < 50) emoji = "🤨";
+  if (percentage === 0) emoji = "🤦‍♂️";
+
   return (
     <p className="result">
+      <span>{emoji}</span>
       You scored <strong>{points}</strong> out of {maxPoints} ({percentage}%)
     </p>
   );
