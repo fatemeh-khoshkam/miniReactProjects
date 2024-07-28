aa93cc7c945c049f9152630f468535555e241883
fatemeh khoshkam <fa.khoshkam@gmail.com>
2024-07-29 00:53:10 +0300

    WIP: displaying city markers on map

--- 10-worldWise/src/components/Map.tsx
+++ 10-worldWise/src/components/Map.tsx
@@ -2,10 +2,15 @@ import styles from "./Map.module.css";
 import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
+import { useCities } from "../contexts/CitiesContext";
+import convertCountryCodeToString from "../utils/convertCountryCodeToString";
 
 function Map() {
   const [mapPosition, setMapPosition] = useState<LatLngExpression>([40, 0]);
+  const { cities } = useCities();
+
   console.log(setMapPosition);
+
   return (
     <div className={styles.mapContainer}>
       <MapContainer
@@ -18,11 +23,21 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <img
+                className={styles.emoji}
+                alt={city.emoji}
+                src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${convertCountryCodeToString(city.emoji)}.svg`}
+              />
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
