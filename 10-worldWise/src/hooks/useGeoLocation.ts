d48e7f577e13c50d2f90504ec780d853c0a6ca0e
fatemeh khoshkam <fa.khoshkam@gmail.com>
2024-07-30 19:05:49 +0300

    WIP: create useGeoLocation hook

---
+++ 10-worldWise/src/hooks/useGeoLocation.ts
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+type Position = {
+  lat: number;
+  lng: number;
+};
+
+interface GeolocationHook {
+  isLoading: boolean;
+  position: Position;
+  error: string | null;
+  getPosition: () => void;
+}
+
+function useGeolocation(
+  defaultPosition: Position = { lat: 0, lng: 0 },
+): GeolocationHook {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [position, setPosition] = useState(defaultPosition);
+  const [error, setError] = useState<string | null>(null);
+
+  function getPosition(): void {
+    if (!navigator.geolocation) {
+      setError("Your browser does not support geolocation");
+      return;
+    }
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos: GeolocationPosition) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error: GeolocationPositionError) => {
+        setError(error.message);
+        setIsLoading(false);
+      },
+    );
+  }
+
+  return { isLoading, position, error, getPosition };
+}
+
+export { useGeolocation };
